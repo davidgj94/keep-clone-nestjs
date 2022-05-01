325c1d6580d81701dfc6e9e27cc939fc98cf12a5
David Gallardo <davidgj1994@gmail.com>
2022-05-01 16:53:32 +0200

    created labels and notes resources, added mongoose and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LabelsModule } from './labels/labels.module';
+import { NotesModule } from './notes/notes.module';
 
 @Module({
-  imports: [],
+  imports: [LabelsModule, NotesModule],
   controllers: [AppController],
   providers: [AppService],
 })
