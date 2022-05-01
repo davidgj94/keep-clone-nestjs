3ca7b7712c7f5eab1c2edb965b24997b901e766b
David Gallardo <davidgj1994@gmail.com>
2022-05-01 17:09:44 +0200

    config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LabelsModule } from './labels/labels.module';
 import { NotesModule } from './notes/notes.module';
+import config from './config';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [LabelsModule, NotesModule],
+  imports: [
+    LabelsModule,
+    NotesModule,
+    ConfigModule.forRoot({ load: [config], isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/index.ts
@@ -0,0 +1,10 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10) || 3000,
+  databaseUrl:
+    process.env.DATABASE_URL || 'mongodb://localhost:27017/keep-clone-nestjs',
+  firebase: {
+    projectId: process.env.FIREBASE_PROJECT_ID || '',
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL || '',
+    privateKey: process.env.FIREBASE_PRIVATE_KEY || '',
+  },
+});
